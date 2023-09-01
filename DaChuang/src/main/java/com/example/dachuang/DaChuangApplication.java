2d730b86317a9e84f95a382baf6c1491ceb0a70f
kankeke1 <1553867108@qq.com>
2023-09-01 13:07:16 +0800

    创建springboot项目

---
+++ DaChuang/src/main/java/com/example/dachuang/DaChuangApplication.java
@@ -0,0 +1,13 @@
+package com.example.dachuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaChuangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaChuangApplication.class, args);
+	}
+
+}
---
+++ DaChuang/src/test/java/com/example/dachuang/DaChuangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dachuang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaChuangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
