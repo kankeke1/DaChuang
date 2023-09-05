ea39279fba594cd6cfed68d342f64000589af17d
kankeke1 <1553867108@qq.com>
2023-09-05 17:45:55 +0800

    Create generateServiceImpl.java

---
+++ DaChuang/src/main/java/com/example/dachuang/service/generateServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.dachuang.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author mmy
+ * @data 2023/9/5 17:41
+ * @description
+ */
+@Component
+public class generateServiceImpl implements generateService{
+@Override
+    public void loadFeatureModel(){
+
+}
+
+@Override
+    public void setTimeNum(int time,int num){
+
+}
+
+@Override
+    public void generateProduct(){
+
+}
+
+@Override
+    public void showProduct(){
+
+}
+
+@Override
+    public void downLoadProduct() {
+
+    }
+}
